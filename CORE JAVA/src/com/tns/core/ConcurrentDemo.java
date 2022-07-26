fca9fd59fe7a2a9a255d70bd72d5d95fa47eebe6
IRAD Bageshwar <IRAD Bageshwar@DESKTOP-LO7DC25>
2022-07-26 12:54:54 +0530

    My First Commit

---
+++ CORE JAVA/src/com/tns/core/ConcurrentDemo.java
@@ -0,0 +1,18 @@
+package com.tns.core;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcurrentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReentrantLock l=new ReentrantLock();  //object 1
+		l.lock();
+		System.out.println(l.isLocked());
+		//l.unlock();
+		System.out.println(l.isHeldByCurrentThread());
+		System.out.println(l.getHoldCount());
+		System.out.println(l.getQueueLength());
+	}
+
+}
