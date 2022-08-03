1cb3265cfa8f3825b4d66144cb3101599777c3b6
IRAD Bageshwar <IRAD Bageshwar@DESKTOP-LO7DC25>
2022-08-03 10:56:56 +0530

    My First Commit

---
+++ JUnitDemo/src/SquareTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class SquareTest {
+
+	@Test
+	void test() {
+		TestingClass t=new TestingClass();
+		int result=t.square(5);}
+
+}
