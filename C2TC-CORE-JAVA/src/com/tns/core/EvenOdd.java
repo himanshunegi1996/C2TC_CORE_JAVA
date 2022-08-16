394b41cfefc0c37414aa4aa680dc9eac63eacefd
pc <pc@192.168.1.40>
2022-08-16 11:32:28 +0530

    My First Commit

---
+++ C2TC-CORE-JAVA/src/com/tns/core/EvenOdd.java
@@ -0,0 +1,23 @@
+package com.tns.core;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args)
+	{
+		Scanner reader=new Scanner(System.in);
+		
+		System.out.println("Enter the number: ");
+		int num = reader.nextInt();	
+		
+		if(num %2==0)
+		{
+			System.out.println(num + " is Even");
+		}
+		else
+		{
+			System.out.println(num + " is Odd");
+		}
+}
+}
